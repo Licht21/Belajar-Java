065e2ad9d9c7529e23f6cae6a13578f599887c46
yusuf <yusuf210409@gmail.com>
2025-06-09 21:52:22 +0700

    Super Constructor

--- src/Manager.java
+++ src/Manager.java
@@ -1,12 +1,20 @@
 class Manager {
     String name;
 
+    Manager(String name) {
+        this.name = name;
+    }
+
     void sayHello(String name) {
         System.out.println("Hello " + name + " From Manager");
     }
 }
 
 class VicePresident extends Manager {
+
+    VicePresident(String name) {
+        super(name);
+    }
     void sayHello(String name) {
         System.out.println("Hello " + name + " From Vice President");
     }
--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,10 +1,14 @@
 public class ManagerApp {
     public static void main(String[] args) {
-        VicePresident vp1 = new VicePresident();
-        Manager mgr1 = new Manager();
+        VicePresident vp1 = new VicePresident("Yusuf");
+        Manager mgr1 = new Manager("Aulia");
 
-        mgr1.sayHello("Yusuf");
+        System.out.println(vp1.name);
+        System.out.println(mgr1.name);
+
         vp1.sayHelloParent("Aulia");
         vp1.sayHello("Aulia");
+
+        mgr1.sayHello("Yusuf");
     }
 }
